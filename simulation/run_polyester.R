a4a5b9475fc4348435ee0e1375bad6734a916f42
Gonzalo Benegas <gbenegas@berkeley.edu>
2022-01-19 11:36:33 -0800

    First pass at pos bias

--- simulation/run_polyester.R
+++ simulation/run_polyester.R
@@ -6,27 +6,41 @@ fasta_path = args[1]
 params_path = args[2]
 n_samples_per_group = strtoi(args[3])
 seed = strtoi(args[4])
-out_dir = args[5]
-print(fasta_path)
+group = args[5]
+bias = args[6]
+out_dir = args[7]
+truncated_fasta_path = args[8]
+
+
+
+if (group == "a") {
+   num_reps = c(n_samples_per_group, 0)
+   bias = "none"
+} else if (group == "b") {
+   num_reps = c(0, n_samples_per_group)
+   seed = seed + 1
+}
+print(group)
+print(num_reps)
+print(bias)
 print(seed)
 
-fasta = readDNAStringSet(fasta_path)
-params = read.table(params_path, header=TRUE, sep="\t")
-print(head(params))
+if (bias == "cdnaf") {
+   fasta_path  = truncated_fasta_path
+}
 
-num_reps = c(n_samples_per_group, n_samples_per_group)
+fasta = readDNAStringSet(fasta_path)
+print(width(fasta))
+params = read.table(params_path, header=TRUE, sep="\t")
+
 fold_changes = cbind(params$fold_change_a, params$fold_change_b)
 readLength = 100
-# readspertx = round(params$coverage * width(fasta) / readLength)
 readspertx = ceiling(params$coverage * width(fasta) / readLength)
-print(head(readspertx))
-print(min(readspertx))
 size = readspertx * params$size_fraction
-print(head(size))
 
 
 simulate_experiment(
     fasta_path, reads_per_transcript=readspertx, num_reps=num_reps,
     fold_changes=fold_changes, outdir=out_dir, seed=seed, error_rate=0.0,
-    strand_specific=TRUE, gzip=TRUE, paired=TRUE, size=size
+    strand_specific=TRUE, gzip=TRUE, paired=TRUE, size=size, bias=bias
 )
